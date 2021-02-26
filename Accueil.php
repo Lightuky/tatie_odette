0043bc170cb236b8d3f4726ac9432cd6a331cc12
Lightuky <finalsmashganondorf@gmail.com>
2021-02-26 11:17:40 +0100

    fixes

--- Accueil.php
+++ Accueil.php
@@ -60,7 +60,7 @@
             <ul class="u-custom-font u-nav u-unstyled u-nav-1">
                 <li class="u-nav-item">
                     <a class="u-button-style u-nav-link u-text-active-white u-text-hover-custom-color-2"
-                       href="Accueil.html" style="padding: 10px 20px;">Accueil</a>
+                       href="Accueil.php" style="padding: 10px 20px;">Accueil</a>
                     <div class="u-nav-popup">
                         <ul class="u-h-spacing-20 u-nav u-unstyled u-v-spacing-10 u-nav-2">
                             <li class="u-nav-item">
@@ -93,7 +93,7 @@
                     <div class="u-menu-close"></div>
                     <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-3">
                         <li class="u-nav-item">
-                            <a class="u-button-style u-nav-link" href="Accueil.html"
+                            <a class="u-button-style u-nav-link" href="Accueil.php"
                                style="padding: 10px 20px;">Accueil</a>
                             <div class="u-nav-popup">
                                 <ul class="u-h-spacing-20 u-nav u-unstyled u-v-spacing-10 u-nav-4">
